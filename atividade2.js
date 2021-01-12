53149c8c7df7ddf163b7e539aa0454bd0fb8d83c
Caetano Nascimento <caetanopark@gmail.com>
2021-01-12 16:25:10 -0300

    tabuada

---
+++ atividade2.js
@@ -0,0 +1,19 @@
+tabuada(1)
+tabuada(2)
+tabuada(3)
+tabuada(4)
+tabuada(5)
+tabuada(6)
+tabuada(7)
+tabuada(8)
+tabuada(9)
+function tabuada(num){    
+    console.log(num)
+    let a = [0,1,2,3,4,5,6,7,8,9,10]
+    for(o = 0; o < a.length; o++){
+        ai = a[num] * o;
+        console.log(a[num] +" * " + o + " = " + ai  )
+    }
+    
+}
+
